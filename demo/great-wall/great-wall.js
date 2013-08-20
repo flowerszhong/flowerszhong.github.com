56ae06d7ff317e6fee4cca02562fe2b749a33379
Matthew Zhong <Matthew.Zhong@morningstar.com>
2013-08-20 19:24:53 +0800

    add great wall chart

---
+++ demo/great-wall/great-wall.js
@@ -0,0 +1,16 @@
+/**
+ * Great Wall
+ * @authors Matthew zhong (flowerszhong@gmail.com)
+ * @date    2013-08-20 10:54:49
+ * @version 0.1
+ */
+
+var GreatWall = function() {
+
+};
+
+GreatWall.prototype = {
+	draw : function () {
+		
+	}
+}
