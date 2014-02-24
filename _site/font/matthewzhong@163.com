cb76f2b52c37e6f1b87f33a7041d34f778868f3b
flowerszhong <flowerszhong@gmail.com>
2014-02-24 21:13:06 +0800

    add old demo

--- _site/font/matthewzhong@163.com
+++
@@ -1,5 +0,0 @@
-Java1986
-
-http://go.codeschool.com/tXvS6Q
-
-username : Node2013/Python2013
---
+++ demo/smart-tip/demo.js
@@ -0,0 +1,42 @@
+'use strict';
+alert(this);
+$(function() {
+	var $container = $("#container"),
+		$btn = $("#btn"),
+		content = "苍井优的出道要追溯到公演于1999年的音乐剧《安妮》, " +
+			"万人候选，她脱颖而出，被选中出演波丽一角。" +
+			"后来她又被选为三井不动产的少女代言人，" +
+			"其清丽的形象一时间变得家喻户晓。";
+	var smartTipInstance = new SmartTip({
+		container: $container,
+		trigger: $btn,
+		content: content,
+
+		borderColor: "#adadad",
+		borderWidth: 1,
+		borderRadius: 5,
+		bgColor: "white",
+		width: 160,
+		height: 200,
+		arrowSize: 10
+	});
+
+	smartTipInstance.show();
+
+	$("#btn").draggable({
+		containment: "parent"
+	});
+
+	$("#btn1").draggable({
+		containment: "parent"
+	});
+	$("#btn").on("dragstop", function(event, ui) {
+		smartTipInstance.attachTo($("#btn"));
+		smartTipInstance.show();
+	});
+
+	$("#btn1").on("dragstop", function(event, ui) {
+		smartTipInstance.attachTo($("#btn1"));
+		smartTipInstance.show();
+	});
+})
